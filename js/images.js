bbee1570d0cebe42c69e957054cc4ebbe536327a
Alessandro Pezzè <igougi.ui@gmail.com>
2016-04-04 16:27:33 +0200

    typo

--- js/images.js
+++ js/images.js
@@ -1,7 +1,7 @@
 var App = angular.module('App', []);
 
 App.controller('Images', function($scope, $http) {
-  $http.get('js/images.json')
+  $http.get('http://naramsim.github.io/Aoe2-Stratega-Uploader/js/images.json#')
        .then(function(res){
           $scope.images = res.data;
         });
