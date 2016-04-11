8a8448dea086732b224d24ce663fb61fbd07d95e
Alessandro Pezzè <igougi.ui@gmail.com>
2016-04-11 09:11:26 +0200

    protocol

--- js/editor.js
+++ js/editor.js
@@ -142,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log(to_send);
 
                 var request = new XMLHttpRequest();
-                request.open('POST', 'betterbin.co/aoe/strategies', true);
+                request.open('POST', 'http://betterbin.co/aoe/strategies', true);
                 request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
                 request.onreadystatechange = function () { 
                     if (request.readyState == 4 && request.status == 200) {
